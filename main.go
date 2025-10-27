43722e7b568d77f2b51f0267684587a813f6c1fd
10357 <yulinwk@gmail.com>
2025-10-27 16:10:04 +0800

    gin範例

--- main.go
+++ main.go
@@ -1,110 +1,132 @@
 package main
 
 import (
+	"net/http"
+	"fmt"
+	"log"
+	"gin/models"
+	"bytes"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
-  "net/http"
-  "database/sql"
-  _ "github.com/denisenkom/go-mssqldb" // 導入 MSSQL 驅動程式
-  "log"
-  "strconv"
-  "strings"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"github.com/xuri/excelize/v2"
 )
+// 資料庫連線變數
+var db *gorm.DB
 
-func main(){
+
+
+// 初始化資料庫
+func initDB() {
+
+	// ⚠️ 修改為你的連線字串
+	server := "192.168.2.182"
+	port := 1433
+	user := "sa"
+	password := "#10357#YuLin"
+	database := "PWB_SLMMS1"
+
+	dsn := fmt.Sprintf(
+		"server=%s;user id=%s;password=%s;port=%d;database=%s;encrypt=disable",
+		server, user, password, port, database,
+	)
+
+	var err error
+	db, err = gorm.Open(sqlserver.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+            TablePrefix:   "",   // 無前綴
+            // SingularTable: true, // 單數表名
+            NoLowerCase:   true, // 禁用小寫轉換
+        },
+	})
+	if err != nil {
+		log.Fatal("❌ 連線 MSSQL 失敗：", err)
+	}
+
+	sqlDB, _ := db.DB()
+	err = sqlDB.Ping()
+	if err != nil {
+		log.Fatal("❌ 資料庫 Ping 失敗：", err)
+	}
+
+	fmt.Println("✅ 成功連線 MSSQL！")
+}
+
+
+func main() {
+	initDB()
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-    connString := "server=192.168.2.182;user id=NTPSLG;password=2B39o182;database=PWB_SLMMS1"
-    // 建立連線
-    db, err := sql.Open("sqlserver", connString)
-    if err != nil {
-        log.Fatal("連線失敗: ", err)
+	router.SetTrustedProxies([]string{})
+
+	router.GET("/mssqlSearchTest", func(c *gin.Context) {
+		// name := c.Query("CP_ACCOUNT")
+		var usr_companies []models.USR_COMPANY
+		if err := db.Find(&usr_companies).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": usr_companies})
+	})
+
+	router.GET("/streetlightExport", func(c *gin.Context) {
+		var streetlightData []models.STREETLIGHT
+		if err := db.Select("CAREA", "SLSN", "SLADD", "CD_SETTYPE","CD_SLMATERIAL","CD_SLWATT", "SLHEIGHT", "SLSDATE", "TWD97X", "TWD97Y", "SLMEMO").Limit(100000).Find(&streetlightData).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		fmt.Println(streetlightData[0])
+		f := excelize.NewFile()
+    defer func() {
+        if err := f.Close(); err != nil {
+            fmt.Println(err)
+        }
+    }()
+
+		// 設定標頭
+    headers := []string{"行政區", "路燈編號", "地址", "材質","燈泡材質","瓦數", "高度", "設立日期", "TWD97X", "TWD97Y", "SLMEMO"}
+    for i, header := range headers {
+        cell := fmt.Sprintf("%s1", string(rune(65+i))) // A1, B1, C1
+        f.SetCellValue("Sheet1", cell, header)
     }
-    defer db.Close()
 
-    router.GET("/sql-select", func(c *gin.Context) {
-        query := `SELECT TOP (1000) [LID]
-                                    ,[SLID]
-                                    ,[LPSN]
-                                    ,[SENSERID]
-                                    ,[MATERIAL]
-                                    ,[WATT]
-                                    ,[IS_HLAM]
-                                    ,[QUEUE]
-                                    ,[RECSTATE]
-                                    ,[CREATETIME]
-                                    ,[UPDATETIME]
-                                FROM [PWB_SLMMS1].[dbo].[LAMP]
-                                where SLID in ('16464', '16465')`
-        rows, err := db.Query(query)
-        if err != nil {
-            log.Fatal("查詢失敗: ", err)
+    // 設定資料
+    for rowIdx, row := range streetlightData {
+				TWD97XStr := strconv.FormatFloat(row.TWD97X, 'f', 6, 64)
+				TWD97YStr := strconv.FormatFloat(row.TWD97Y, 'f', 6, 64)
+        values := []interface{}{row.CAREA, row.SLSN, row.SLADD, row.CD_SETTYPE,row.CD_SLMATERIAL,row.CD_SLWATT, row.SLHEIGHT, row.SLSDATE, TWD97XStr, TWD97YStr, row.SLMEMO}
+        for colIdx, val := range values {
+            cell := fmt.Sprintf("%s%d", string(rune(65+colIdx)), rowIdx+2) // A2, B2, C2
+            f.SetCellValue("Sheet1", cell, val)
         }
-        defer rows.Close()
+    }
 
-        // 儲存查詢結果
-        var results []map[string]interface{}
-        for rows.Next() {
-            var lid int
-            var slid, lpsn, senserid, material, watt, isHlam, queue, recstate *string
-            var createTime, updateTime sql.NullTime
+    // 儲存檔案
+    if err := f.SaveAs("C:\\ApacheServer\\WEBPHP8.3.10\\SL_Main\\storage\\app\\public\\tmpdownload\\output.xlsx"); err != nil {
+        fmt.Println(err)
+    }
 
-            // 掃描所有欄位
-            if err := rows.Scan(&lid, &slid, &lpsn, &senserid, &material, &watt, &isHlam, &queue, &recstate, &createTime, &updateTime); err != nil {
-                c.JSON(http.StatusInternalServerError, gin.H{"error": "掃描結果失敗: " + err.Error()})
-                return
-            }
+		// 將 Excel 檔案寫入緩衝區
+		var buf bytes.Buffer
+		if err := f.Write(&buf); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate Excel file"})
+			return
+		}
+		// 設置 HTTP 標頭以觸發下載
+		c.Header("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+		c.Header("Content-Disposition", "attachment; filename=streetlight_data.xlsx")
+		c.Header("Content-Length", fmt.Sprintf("%d", buf.Len()))
 
-            // 處理時間欄位
-            createTimeStr := ""
-            if createTime.Valid {
-                createTimeStr = createTime.Time.Format("2006-01-02 15:04:05")
-            }
-            updateTimeStr := ""
-            if updateTime.Valid {
-                updateTimeStr = updateTime.Time.Format("2006-01-02 15:04:05")
-            }
+    fmt.Println("Excel 檔案已匯出：output.xlsx")
 
-            // 字串拼接（使用 strings.Builder 提高效率）
-            var builder strings.Builder
-            builder.WriteString(strconv.Itoa(lid))
-            builder.WriteString("-")
-            builder.WriteString(*lpsn)
-            message := builder.String()
+		// 回傳二進位資料給前端
+		c.Data(http.StatusOK, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", buf.Bytes())
 
-            // 儲存結果
-            results = append(results, map[string]interface{}{
-                "lid":         lid,
-                "slid":        slid,
-                "lpsn":        lpsn,
-                "senserid":    senserid,
-                "material":    material,
-                "watt":        watt,
-                "isHlam":      isHlam,
-                "queue":       queue,
-                "recstate":    recstate,
-                "createTime":  createTimeStr,
-                "updateTime":  updateTimeStr,
-                "message":     message,
-            })
-        }
+		// c.JSON(http.StatusOK, gin.H{"msg": "Excel 檔案已匯出"})
+	})
 
-        // 檢查查詢錯誤
-        if err = rows.Err(); err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": "查詢處理錯誤: " + err.Error()})
-            return
-        }
-
-
-        // 返回結果
-        c.JSON(http.StatusOK, gin.H{
-            "data":         results,
-        })
-    })
-
-    router.GET("/youtube-download", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "data":         "https://www.youtube.com/watch?v=QH2_TGUlwu4",
-        })
-    })
-
-  router.Run(":8080") // listen and serve on 0.0.0.0:8080
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
---
+++ models/models.go
@@ -0,0 +1,116 @@
+package models
+
+type USR_COMPANY struct {
+	CPID int `json:"CPID" gorm:"column:ID;primaryKey"`
+	CP_ACCOUNT string `json:"CP_ACCOUNT", gorm:"column:CP_ACCOUNT"`
+	CP_PWD string `json:"CP_PWD", gorm:"column:CP_PWD"`
+	DTID int `json:"DTID", gorm:"column:DTID"`
+	STATUS int `json:"STATUS", gorm:"column:STATUS"`
+	MOD_DATETIME string `json:"MOD_DATETIME", gorm:"column:MOD_DATETIME"`
+	DATELINE_DATETIME string `json:"DATELINE_DATETIME", gorm:"column:DATELINE_DATETIME"`
+	CREATE_DATETIME string `json:"CREATE_DATETIME", gorm:"column:CREATE_DATETIME"`
+	ISPFI int `json:"ISPFI", gorm:"column:ISPFI"`
+	IDOLD string `json:"IDOLD", gorm:"column:IDOLD"`
+	BIND_AREA_CTYPE string `json:"BIND_AREA_CTYPE", gorm:"column:BIND_AREA_CTYPE"`
+	TMP_PWD string `json:"TMP_PWD", gorm:"column:TMP_PWD"`
+	TMP_TIME string `json:"TMP_TIME", gorm:"column:TMP_TIME"` 
+}
+
+func (USR_COMPANY) TableName() string {
+    return "USR_COMPANY" // ⚠️ 改成你在 SQL Server 實際的表名
+}
+
+type STREETLIGHT struct{
+	SLID int `json:"SLID" gorm:"column:SLID;primaryKey"`
+	CAREA string `json:"CAREA", gorm:"column:CAREA"`
+	SLPWBOXID string `json:"SLPWBOXID", gorm:"column:SLPWBOXID"`
+	CD_DIRECTION string `json:"CD_DIRECTION", gorm:"column:CD_DIRECTION"`
+	CD_SETTYPE string `json:"CD_SETTYPE", gorm:"column:CD_SETTYPE"`
+	CD_SLMATERIAL string `json:"CD_SLMATERIAL", gorm:"column:CD_SLMATERIAL"`
+	CD_SLWATT string `json:"CD_SLWATT", gorm:"column:CD_SLWATT"`
+	CD_STREET string `json:"CD_STREET", gorm:"column:CD_STREET"`
+	CD_VILLAGE string `json:"CD_VILLAGE", gorm:"column:CD_VILLAGE"`
+	SLSN string `json:"SLSN", gorm:"column:SLSN"`
+	SLSNNO string `json:"SLSNNO", gorm:"column:SLSNNO"`
+	SLADD string `json:"SLADD", gorm:"column:SLADD"`
+	SLADDDUAN string `json:"SLADDDUAN", gorm:"column:SLADDDUAN"`
+	SLADDLIN string `json:"SLADDLIN", gorm:"column:SLADDLIN"`
+	SLADDALLEY string `json:"SLADDALLEY", gorm:"column:SLADDALLEY"`
+	SLADDLANE string `json:"SLADDLANE", gorm:"column:SLADDLANE"`
+	SLADDNO string `json:"SLADDNO", gorm:"column:SLADDNO"`
+	SLDISTANCE string `json:"SLDISTANCE", gorm:"column:SLDISTANCE"`
+	SLHEIGHT string `json:"SLHEIGHT", gorm:"column:SLHEIGHT"`
+	SLPOLENO string `json:"SLPOLENO", gorm:"column:SLPOLENO"`
+	COMPID string `json:"COMPID", gorm:"column:COMPID"`
+	SLSDATE string `json:"SLSDATE", gorm:"column:SLSDATE"`
+	SLLIFE string `json:"SLLIFE", gorm:"column:SLLIFE"`
+	SLPDATE string `json:"SLPDATE", gorm:"column:SLPDATE"`
+	SLBULB string `json:"SLBULB", gorm:"column:SLBULB"`
+	SLMEMO string `json:"SLMEMO", gorm:"column:SLMEMO"`
+	SLMATER string `json:"SLMATER", gorm:"column:SLMATER"`
+	MTTIMES string `json:"MTTIMES", gorm:"column:MTTIMES"`
+	SL_CUTOUT string `json:"SL_CUTOUT", gorm:"column:SL_CUTOUT"`
+	SL_DM string `json:"SL_DM", gorm:"column:SL_DM"`
+	SL_GROUND string `json:"SL_GROUND", gorm:"column:SL_GROUND"`
+	SL_LEAKWATER string `json:"SL_LEAKWATER", gorm:"column:SL_LEAKWATER"`
+	SL_AIR string `json:"SL_AIR", gorm:"column:SL_AIR"`
+	SL_MONITOR string `json:"SL_MONITOR", gorm:"column:SL_MONITOR"`
+	SL_LEAKPOWER string `json:"SL_LEAKPOWER", gorm:"column:SL_LEAKPOWER"`
+	PRJNID string `json:"PRJNID", gorm:"column:PRJNID"`
+	LMSKID string `json:"LMSKID", gorm:"column:LMSKID"`
+	COVER string `json:"COVER", gorm:"column:COVER"`
+	SL_STYLE string `json:"SL_STYLE", gorm:"column:SL_STYLE"`
+	SL_STYLENO string `json:"SL_STYLENO", gorm:"column:SL_STYLENO"`
+	DG_CORR string `json:"DG_CORR", gorm:"column:DG_CORR"`
+	DG_LOSE string `json:"DG_LOSE", gorm:"column:DG_LOSE"`
+	DG_CORD string `json:"DG_CORD", gorm:"column:DG_CORD"`
+	DG_SCREEN string `json:"DG_SCREEN", gorm:"column:DG_SCREEN"`
+	DG_LTUB string `json:"DG_LTUB", gorm:"column:DG_LTUB"`
+	TWD67X string `json:"TWD67X", gorm:"column:TWD67X"`
+	TWD67Y string `json:"TWD67Y", gorm:"column:TWD67Y"`
+	TWD97X float64 `json:"TWD97X", gorm:"column:TWD97X"`
+	TWD97Y float64 `json:"TWD97Y", gorm:"column:TWD97Y"`
+	LONGITUDE string `json:"LONGITUDE", gorm:"column:LONGITUDE"`
+	LATITUDE string `json:"LATITUDE", gorm:"column:LATITUDE"`
+	STATE string `json:"STATE", gorm:"column:STATE"`
+	FIXAREA string `json:"FIXAREA", gorm:"column:FIXAREA"`
+	RECSTATE string `json:"RECSTATE", gorm:"column:RECSTATE"`
+	FLAG string `json:"FLAG", gorm:"column:FLAG"`
+	FLAGMEMO string `json:"FLAGMEMO", gorm:"column:FLAGMEMO"`
+	DISDATE string `json:"DISDATE", gorm:"column:DISDATE"`
+	SLFROM string `json:"SLFROM", gorm:"column:SLFROM"`
+	OLDSLSN string `json:"OLDSLSN", gorm:"column:OLDSLSN"`
+	HLAM_CATY string `json:"HLAM_CATY", gorm:"column:HLAM_CATY"`
+	HLAM_POWER string `json:"HLAM_POWER", gorm:"column:HLAM_POWER"`
+	HLAM_CNT string `json:"HLAM_CNT", gorm:"column:HLAM_CNT"`
+	HLAM_SEC_DT string `json:"HLAM_SEC_DT", gorm:"column:HLAM_SEC_DT"`
+	AD_LINE string `json:"AD_LINE", gorm:"column:AD_LINE"`
+	NEWSLSN string `json:"NEWSLSN", gorm:"column:NEWSLSN"`
+	SLTYPE string `json:"SLTYPE", gorm:"column:SLTYPE"`
+	SLKIND string `json:"SLKIND", gorm:"column:SLKIND"`
+	SLEDATE string `json:"SLEDATE", gorm:"column:SLEDATE"`
+	LANDMARK string `json:"LANDMARK", gorm:"column:LANDMARK"`
+	SET_MATERIAL string `json:"SET_MATERIAL", gorm:"column:SET_MATERIAL"`
+	PRT_COMP string `json:"PRT_COMP", gorm:"column:PRT_COMP"`
+	ISMAINSTREET string `json:"ISMAINSTREET", gorm:"column:ISMAINSTREET"`
+	DENY_MAINTAIN string `json:"DENY_MAINTAIN", gorm:"column:DENY_MAINTAIN"`
+	APPLY_FOR_ADD string `json:"APPLY_FOR_ADD", gorm:"column:APPLY_FOR_ADD"`
+	DISDESC string `json:"DISDESC", gorm:"column:DISDESC"`
+	DISUID string `json:"DISUID", gorm:"column:DISUID"`
+	SLADDLONG string `json:"SLADDLONG", gorm:"column:SLADDLONG"`
+	SLSNMEMO string `json:"SLSNMEMO", gorm:"column:SLSNMEMO"`
+	USLSN string `json:"USLSN", gorm:"column:USLSN"`
+	UDATE string `json:"UDATE", gorm:"column:UDATE"`
+	IOT_STATUS string `json:"IOT_STATUS", gorm:"column:IOT_STATUS"`
+	IOT_REPORT_ID string `json:"IOT_REPORT_ID", gorm:"column:IOT_REPORT_ID"`
+	IOT_REPORT_AT string `json:"IOT_REPORT_AT", gorm:"column:IOT_REPORT_AT"`
+	IS_ENERGY_SAVING string `json:"IS_ENERGY_SAVING", gorm:"column:IS_ENERGY_SAVING"`
+	IS_AI_CONTROL string `json:"IS_AI_CONTROL", gorm:"column:IS_AI_CONTROL"`
+	IS_SMART_POLE string `json:"IS_SMART_POLE", gorm:"column:IS_SMART_POLE"`
+	EESW string `json:"EESW", gorm:"column:EESW"`
+	DIS_OFFICIAL_DOCUMENT string `json:"DIS_OFFICIAL_DOCUMENT", gorm:"column:DIS_OFFICIAL_DOCUMENT"`
+}
+
+func (STREETLIGHT) TableName() string {
+		return "STREETLIGHT" // ⚠️ 改成你在 SQL Server 實際的表名
+}
